c5a3e598a3fcf05a35f64415c7060870aae7786d
GroupAVI Database <a6_cpe231@cpe.kmutt.ac.th>
2022-05-02 17:00:06 +0000

    Auto commit by CyberPanel Daily cron on 05-02-2022_17-00-03

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "a6_rosehat";
+$password = "a6_rosehat_kass";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
